960f2a98dbed108000ea718b0933f9da25a96bf2
Luis Estevam Rosa Chaves <luisestevam33@gmail.com>
2024-11-25 18:50:32 -0300

    feat: add lista5

---
+++ Lista5/app/src/main/java/com/example/lista5/ui/theme/CounterAppTheme.kt
@@ -0,0 +1,39 @@
+package com.example.counterapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.example.lista5.ui.theme.typography
+import com.example.lista5.ui.theme.Shapes
+
+private val DarkColorPalette = darkColors(
+    primary = Color(0xFFBB86FC),
+    primaryVariant = Color(0xFF3700B3),
+    secondary = Color(0xFF03DAC6)
+)
+
+private val LightColorPalette = lightColors(
+    primary = Color(0xFF6200EE),
+    primaryVariant = Color(0xFF3700B3),
+    secondary = Color(0xFF03DAC6)
+)
+
+@Composable
+fun CounterAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = typography,
+        shapes = Shapes,
+        content = content
+    )
+}
---
+++ Lista5/app/src/main/java/com/example/lista5/ui/theme/Typography.kt
@@ -0,0 +1,9 @@
+package com.example.lista5.ui.theme
+
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.font.FontFamily
+
+val typography = Typography(
+    defaultFontFamily = FontFamily.Default
+)
